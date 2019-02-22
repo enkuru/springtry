d21181660a36c06e0bf1ff46d75267783e69d88d
Enes KURU <enes793k@icloud.com>
2019-02-22 14:17:28 +0300

    first-commit

---
+++ src/main/java/com/enkuru/springtry/SpringtryApplication.java
@@ -0,0 +1,13 @@
+package com.enkuru.springtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtryApplication.class, args);
+	}
+
+}
